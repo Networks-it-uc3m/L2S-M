35df3007b2292f4da79ac3d0327f4f30e6f32511
Alex ubuntu vm <alexdecb@yahoo.es>
2024-04-15 12:54:47 +0200

    switch: adjusted images and building process to new version

--- build/switch/build-go.sh
+++ build/switch/build-go.sh
@@ -10,3 +10,4 @@ fi
 
 go build -v -o "${DEST_DIR}"/l2sm-init ./cmd/l2sm-init 
 go build -v -o "${DEST_DIR}"/l2sm-vxlans ./cmd/l2sm-vxlans 
+go build -v -o "${DEST_DIR}"/l2sm-add-port ./cmd/l2sm-add-port 
--- src/switch/cmd/l2sm-add-port/main.go
+++ src/switch/cmd/l2sm-add-port/main.go
@@ -1,19 +1,11 @@
 package main
 
 import (
-	"encoding/json"
 	"errors"
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"ovs-switch/pkg/ovs"
 )
-
-type Node struct {
-	Name          string `json:"name"`
-	NodeIP        string `json:"nodeIP"`
-	NeighborNodes []Node `json:"neighborNodes"`
-}
 
 // Script that takes two required arguments:
 // the first one is the name in the cluster of the node where the script is running
@@ -49,50 +41,4 @@ func takeArguments() (string, error) {
 	}
 
 	return *portName, nil
-}
-
-func createTopology(bridge ovs.Bridge, nodes []Node, nodeName string) error {
-
-	// Search for the corresponding node in the configuration, according to the first passed parameter.
-	// Once the node is found, create a bridge for every neighbour node defined.
-	// The bridge is created with the nodeIp and neighborNodeIP and VNI. The VNI is generated in the l2sm-controller thats why its set to 'flow'.
-	for _, node := range nodes {
-		if node.Name == nodeName {
-			//nodeIP := strings.TrimSpace(node.NodeIP)
-			connectToNeighbors(bridge, node)
-		}
-	}
-	return nil
-}
-
-func readFile(configDir string) ([]Node, error) {
-	/// Read file and save in memory the JSON info
-	data, err := ioutil.ReadFile(configDir)
-	if err != nil {
-		fmt.Println("No input file was found.", err)
-		return nil, err
-	}
-
-	var nodes []Node
-	err = json.Unmarshal(data, &nodes)
-	if err != nil {
-		return nil, err
-	}
-
-	return nodes, nil
-
-}
-
-func connectToNeighbors(bridge ovs.Bridge, node Node) error {
-	for vxlanNumber, neighbor := range node.NeighborNodes {
-		vxlanId := fmt.Sprintf("vxlan%d", vxlanNumber)
-		err := bridge.CreateVxlan(ovs.Vxlan{VxlanId: vxlanId, LocalIp: node.NodeIP, RemoteIp: neighbor.NodeIP, UdpPort: "7000"})
-
-		if err != nil {
-			return fmt.Errorf("could not create vxlan between node %s and node %s", node.Name, neighbor)
-		} else {
-			fmt.Printf("Created vxlan between node %s and node %s.\n", node.Name, neighbor)
-		}
-	}
-	return nil
 }
