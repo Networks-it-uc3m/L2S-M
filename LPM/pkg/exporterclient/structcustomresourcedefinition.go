78b93f6edec2202d605a213e826f75785174f9be
Alex Tjaarda <alexdecb@yahoo.es>
2025-05-05 14:02:34 +0200

    removed lpm as a standalone directory

--- LPM/pkg/exporterclient/structcustomresourcedefinition.go
+++
@@ -1,9 +0,0 @@
-package exporterclient
-
-import (
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
-)
-
-type StructCustomResourceDefinition interface {
-	GetUnstructuredData() *unstructured.Unstructured
-}
